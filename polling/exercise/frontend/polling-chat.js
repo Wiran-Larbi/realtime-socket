da107f17c9f2a8c03a6805a692622c3d3f5b7c6e
DotCipher <dotcipher@mail.dev>
2023-09-28 13:56:02 +0100

    first commit

---
+++ polling/exercise/frontend/polling-chat.js
@@ -0,0 +1,91 @@
+const chat = document.getElementById("chat");
+const msgs = document.getElementById("msgs");
+
+// let's store all current messages here
+let allChat = [];
+
+// the interval to poll at in milliseconds
+const INTERVAL = 3000;
+
+// a submit listener on the form in the HTML
+chat.addEventListener("submit", function (e) {
+  e.preventDefault();
+  postNewMsg(chat.elements.user.value, chat.elements.text.value);
+  chat.elements.text.value = "";
+});
+
+async function postNewMsg(user, text) {
+  // post to /poll a new message
+  // write code here
+  const data = {
+    user,
+    text
+    // time: Date.now(),
+  }
+  const options = {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json"
+    }
+  }
+
+  await fetch("/poll", options);
+  // const json = await res.json();
+
+
+}
+
+async function getNewMsgs() {
+  // poll the server
+  // write code here
+  let json;
+  let failedTries;
+  try {
+    const res = await fetch("/poll");
+    json = await res.json();
+
+    if (res.status >= 400) {
+      throw new Error("request not succeed: " + res.status);
+    }
+    
+      allChat = json.msg;
+      render();
+      failedTries = 0;
+  } catch(err) {
+    // backoff code
+    console.error("polling error: ", err);
+    failedTries++;
+  }
+  // setTimeout(getNewMsgs, INTERVAL);
+
+}
+
+function render() {
+  // as long as allChat is holding all current messages, this will render them
+  // into the ui. yes, it's inefficent. yes, it's fine for this example
+  const html = allChat.map(({ user, text, time, id }) =>
+    template(user, text, time, id)
+  );
+  msgs.innerHTML = html.join("\n");
+}
+
+// given a user and a msg, it returns an HTML string to render to the UI
+const template = (user, msg) =>
+  `<li class="collection-item"><span class="badge">${user}</span>${msg}</li>`;
+
+// make the first request
+// getNewMsgs();
+const BACKOFF = 5000;
+let timeToMakeNextRequest = 0;
+let failedTries = 0;
+async function rafTimer(time) {
+  // console.log(time);
+  if (timeToMakeNextRequest <= time){
+    await getNewMsgs();
+    timeToMakeNextRequest = time + INTERVAL + failedTries * BACKOFF;
+  }
+
+  requestAnimationFrame(rafTimer);
+}
+requestAnimationFrame(rafTimer)
