4f0ace0dbb91f8246b862a55f03deb276511c573
DotCipher <dotcipher@mail.dev>
2023-09-28 18:10:46 +0100

    sockets lecture

---
+++ websockets/exercise-socketio/frontend/socketio-chat.js
@@ -0,0 +1,43 @@
+// a global called "io" is being loaded separately
+import { io } from "../backend/server.js";
+
+const chat = document.getElementById("chat");
+const msgs = document.getElementById("msgs");
+const presence = document.getElementById("presence-indicator");
+
+let allChat = [];
+const socket = io("http://localhost:8080");
+
+socket.on("connect", () => {
+  console.log("connected ..");
+  presence.innerText = "🟢";
+});
+
+socket.on("disconnect", () => {
+  console.log("disconnected ..");
+  presence.innerText = "🔴";
+});
+
+
+
+chat.addEventListener("submit", function (e) {
+  e.preventDefault();
+  postNewMsg(chat.elements.user.value, chat.elements.text.value);
+  chat.elements.text.value = "";
+});
+
+async function postNewMsg(user, text) {
+  /*
+   *
+   * Code goes here
+   *
+   */
+}
+
+function render() {
+  const html = allChat.map(({ user, text }) => template(user, text));
+  msgs.innerHTML = html.join("\n");
+}
+
+const template = (user, msg) =>
+  `<li class="collection-item"><span class="badge">${user}</span>${msg}</li>`;
